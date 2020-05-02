25161fe3b05d4a18484300e9e2a0e3bbbdd80592
Gabriel Touron <56262724+GabrielTouron@users.noreply.github.com>
2020-05-02 15:36:22 +0200

    Create Wilder.java

---
+++ Wilder.java
@@ -0,0 +1,36 @@
+public class Wilder {
+	//attributes
+	String firstname;
+	boolean aware;
+	
+	//constructor
+	public Wilder(String firstname, boolean aware) {
+		this.firstname = firstname;
+		this.aware = aware;
+	}
+	
+	//getter
+	public String getFirstname() {
+		return this.firstname;
+	}
+	public boolean isAware() {
+		return this.aware;
+	}
+	
+	//setter
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public void setAware(boolean aware) {
+		this.aware = aware;
+	}
+	
+	// instance method
+	public String whoAmI() {
+		if (this.isAware()) {
+			return "Je m'appelle " + this.getFirstname() + " et je suis aware";
+		} else {
+			return "Je m'appelle " + this.getFirstname() + " et je ne suis pas aware";
+		}
+	}
+}
